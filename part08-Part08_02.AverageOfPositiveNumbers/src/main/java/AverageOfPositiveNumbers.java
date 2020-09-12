74433a01c590c85966f9b8ca1eb4696d215d3702
raymondwu22 <raywupharmd@outlook.com>
2020-09-11 22:58:12 -0400

    complete 8-1 and 8-2

--- part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,23 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+		int sum = 0;
+		int count = 0;
+		while (true) {
+			int input = Integer.valueOf(scanner.nextLine());
+			if (input == 0) {
+				break;
+			}
 
+			if (input > 0) {
+				sum += input;
+				count++;
+			}
+		}
+		if (count == 0) {
+			System.out.println("Cannot calculate the average");
+		} else {
+			System.out.println((double) sum / count);
+		}
     }
 }
