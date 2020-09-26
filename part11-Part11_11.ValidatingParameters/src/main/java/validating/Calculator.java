05fee8b4831cc7d09dfa11a7a7cb6cd47f298dc9
raymondwu22 <raywupharmd@outlook.com>
2020-09-26 00:38:59 -0400

    complete activity 11-11

--- part11-Part11_11.ValidatingParameters/src/main/java/validating/Calculator.java
+++ part11-Part11_11.ValidatingParameters/src/main/java/validating/Calculator.java
@@ -3,7 +3,9 @@ package validating;
 public class Calculator {
 
     public int factorial(int num) {
-
+		if (num < 0) {
+			throw new IllegalArgumentException("Number must be positive.");
+		}
         int answer = 1;
         for (int i = 1; i <= num; i++) {
             answer *= i;
@@ -13,7 +15,9 @@ public class Calculator {
     }
 
     public int binomialCoefficent(int setSize, int subsetSize) {
-
+		if (subsetSize > setSize) {
+			throw new IllegalArgumentException("subset must be smaller than the set!");
+		}
         int numerator = factorial(setSize);
         int denominator = factorial(subsetSize) * factorial(setSize - subsetSize);
 
--- part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
+++ part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
@@ -6,7 +6,11 @@ public class Person {
     private int age;
 
     public Person(String name, int age) {
-
+    	if (name == null || name.isEmpty() || name.length() > 40) {
+			throw new IllegalArgumentException("Name must not be empty, null or greater than 40 characters!");
+		} else if ( age < 0 || age > 120) {
+			throw new IllegalArgumentException("age out of range");
+		}
         this.name = name;
         this.age = age;
     }
